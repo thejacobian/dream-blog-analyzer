75bb11789985bdbbec1bfa7099a19108602bd115
Sami Austin <samicaustin@gmail.com>
2019-04-24 13:29:56 -0600

    Added index, show, edit, and new routes

--- controllers/dreamController.js
+++ controllers/dreamController.js
@@ -3,6 +3,8 @@ const router = express.Router();
 // const User = require('../models/users');
 const Dream = require('../models/dreams');
 
+
+// INDEX ROUTE
 router.get('/', async (req, res) => {
     try {
         const allDreams = Dream.find();
@@ -13,5 +15,41 @@ router.get('/', async (req, res) => {
         res.send(err)
     };
 });
+
+// SHOW ROUTE
+router.get('/:id', async (req, res) => {
+    try {
+        const oneDream = Dream.findById(req.params.id);
+        res.render('dreams/show.ejs', {
+            dream: oneDream
+        })
+    }catch(err){
+        res.send(err)
+    }
+});
+
+// NEW ROUTE
+router.get('/new', (req, res) => {
+    res.render('dreams/new.ejs')
+});
+
+// CREATE ROUTE
+
+// EDIT ROUTE
+router.get('/:id/edit', (req, res) => {
+    try {
+        const editDream = Dream.findById(req.params.id);
+        res.render('dreams/new.ejs', {
+            dream: editDream
+        })
+    } catch(err){
+        res.send(err)
+    }
+});
+
+// UPDATE ROUTE
+
+// DELETE ROUTE
+
 
 module.exports = router;
