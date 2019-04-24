95cf42b7595632199686a6b800e8df182bf154a1
Destiny <dshillis1@gmail.com>
2019-04-24 15:14:48 -0600

    User registration ejs and get route

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,4 +3,9 @@ const router = express.Router();
 const User = require('../models/users');
 const bcrypt = require('bcryptjs');
 
+router.get('/register', async (req, res) => {
+    res.render('auth/register.ejs');
+});
+
+
 module.exports = router;
